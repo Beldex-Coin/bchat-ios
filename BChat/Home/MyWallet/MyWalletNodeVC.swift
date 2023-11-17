cc38efd4747cc056a50ddbfcd07e21e90ef36061
johnbradsfiled <109148195+johnbradsfiled@users.noreply.github.com>
2023-11-17 16:42:48 +0530

    Node screen issue and minor fixes

--- BChat/Home/MyWallet/MyWalletNodeVC.swift
+++ BChat/Home/MyWallet/MyWalletNodeVC.swift
@@ -442,7 +442,7 @@ extension MyWalletNodeVC: UICollectionViewDataSource, UICollectionViewDelegate, 
                 cell.mainView.layer.backgroundColor = UIColor(red: 35.0/255, green: 130.0/255, blue: 244.0/255, alpha: 1.0).cgColor
                 cell.isUserInteractionEnabled = false
             }
-        } else if (nodeArray.count == 7) {
+        } else if (nodeArray.count == 6) {
             if(nodeArray[indexPath.row] == randomNodeValue) {
                 cell.mainView.layer.backgroundColor = UIColor(red: 35.0/255, green: 130.0/255, blue: 244.0/255, alpha: 1.0).cgColor
                 cell.isUserInteractionEnabled = false
