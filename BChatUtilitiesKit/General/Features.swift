41d1ab876494efd206bd65e48ddaf134d04e9b8b
ios-wingman <147593515+ios-wingman@users.noreply.github.com>
2024-06-07 08:36:44 +0530

    custom compilation issue fixed

--- BChatUtilitiesKit/General/Features.swift
+++ BChatUtilitiesKit/General/Features.swift
@@ -5,7 +5,12 @@ public final class Features : NSObject {
     /// Specifies the useOnionRequests for the API
     public static let useOnionRequests = true
     
-    // MAINNET
     /// Specifies the Server public key for the API
-    public static var isTestNet = false
+    public static var isTestNet: Bool {
+        #if TESTNET
+            return true
+        #else
+            return false
+        #endif
+    }
 }
