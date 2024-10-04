c4759be7446559dfe2e399d30e85c201a0f556e1
ios-wingman <147593515+ios-wingman@users.noreply.github.com>
2024-10-04 18:18:40 +0530

    removed explorer and maninnet

--- BChat/Home/MyWallet/MyWalletHomeVC.swift
+++ BChat/Home/MyWallet/MyWalletHomeVC.swift
@@ -63,7 +63,8 @@ class MyWalletHomeVC: UIViewController, ExpandedCellDelegate,UITextFieldDelegate
     //========================================================================================
 //    ,"publicnode5.rpcnode.stream:29095"
     // MAINNET
-    var nodeArray = ["explorer.beldex.io:19091","mainnet.beldex.io:29095","publicnode1.rpcnode.stream:29095","publicnode2.rpcnode.stream:29095","publicnode3.rpcnode.stream:29095","publicnode4.rpcnode.stream:29095","publicnode5.rpcnode.stream:29095"] //["149.102.156.174:19095"]
+    //explorer.beldex.io:19091","mainnet.beldex.io:29095",
+    var nodeArray = ["publicnode1.rpcnode.stream:29095","publicnode2.rpcnode.stream:29095","publicnode3.rpcnode.stream:29095","publicnode4.rpcnode.stream:29095","publicnode5.rpcnode.stream:29095"] //["149.102.156.174:19095"]
     
     // TESTNET
 //    var nodeArray = ["149.102.156.174:19095"]
