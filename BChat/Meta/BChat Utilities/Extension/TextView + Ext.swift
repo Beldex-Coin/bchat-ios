5d3a74da50ba2a2ea9e400e4c4c65fb06443de33
ios-wingman <147593515+ios-wingman@users.noreply.github.com>
2025-06-18 12:11:44 +0530

    New chat popup: The cursor is positioned on top of the placeholder text.

--- BChat/Meta/BChat Utilities/Extension/TextView + Ext.swift
+++ BChat/Meta/BChat Utilities/Extension/TextView + Ext.swift
@@ -9,7 +9,7 @@ extension UITextView{
         placeholderLabel.font = Fonts.OpenSans(ofSize: 14)
         placeholderLabel.sizeToFit()
         placeholderLabel.tag = 222
-        placeholderLabel.frame.origin = CGPoint(x: 2, y: 8)
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: 8)
         placeholderLabel.textColor = UIColor(hex: 0xA7A7BA)
         placeholderLabel.isHidden = !self.text.isEmpty
         self.addSubview(placeholderLabel)
