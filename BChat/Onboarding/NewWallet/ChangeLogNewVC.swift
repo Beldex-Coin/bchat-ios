ec93f8fe6ffaa8b7d48878133541ce4ec9ad9797
ios-wingman <147593515+ios-wingman@users.noreply.github.com>
2025-10-27 13:49:34 +0530

    updated change log

--- BChat/Onboarding/NewWallet/ChangeLogNewVC.swift
+++ BChat/Onboarding/NewWallet/ChangeLogNewVC.swift
@@ -18,7 +18,7 @@ class ChangeLogNewVC: BaseVC, UITableViewDataSource, UITableViewDelegate{
         return result
     }()
     
-    var versionArray = ["1.0.0", "1.1.0", "1.2.0", "1.2.1", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.4.0", "1.4.1", "1.4.2", "1.4.4", "1.4.5", "1.5.0", "1.6.0", "1.6.1", "1.6.2"]
+    var versionArray = ["1.0.0", "1.1.0", "1.2.0", "1.2.1", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.4.0", "1.4.1", "1.4.2", "1.4.4", "1.4.5", "1.5.0", "1.6.0", "1.6.1", "1.6.2", "1.7.0"]
     var descArray = ["\u{2022} Initial release\n\u{2022} Added changelog",
                      "\u{2022} Message request implementation\n\u{2022} Link Preview will be turn on by default\n\u{2022} Add the images for SwipeActionsConfiguration",
                      "\u{2022} Call Feature Added\n\u{2022} Blocked Contact list added\n\u{2022} Minor Bug Fixes",
@@ -35,8 +35,9 @@ class ChangeLogNewVC: BaseVC, UITableViewDataSource, UITableViewDelegate{
                      "\u{2022} Revamped User experience \n\u{2022} User can link their BNS name and get verified badge \n\u{2022} Improved node connectivity for better synchronization \n\u{2022} Improved overall performance of the app \n\u{2022} Improved search functionality in home screen \n\u{2022} User can block a user on message request \n\u{2022} Minor bug fixes",
                      "\u{2022} Introducing emoji reaction feature \n\u{2022} Added message archive - unarchive functionality \n\u{2022} Updated secret group info \n\u{2022} User can swap the videos in video calls \n\u{2022} Fixes crash in calls \n\u{2022} Minor bug fixes",
                      "\u{2022} Users can now share Media from any apps to BChat \n\u{2022} Introduced GIF Feature \n\u{2022} Fixed bugs in message request flow \n\u{2022} Fixed issues in chat screen \n\u{2022} Minor bug fixes",
-                     "\u{2022} Minor bug fixes",]
-    var expandedRowArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+                     "\u{2022} Minor bug fixes",
+                     "\u{2022} Introduced share contact feature \n\u{2022} Introduced screenshot notify feature \n\u{2022} Minor bug fixes",]
+    var expandedRowArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
     
     
     override func viewDidLoad() {
