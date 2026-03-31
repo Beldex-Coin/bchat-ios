12af5d06206f98006dc6134df927208429f4d7b2
ios-wingman <147593515+ios-wingman@users.noreply.github.com>
2026-03-31 19:07:17 +0530

    Fix: Archived chats still receive notifications when “Keep Chats Archived” is enabled

--- BChat/Notifications/AppNotifications.swift
+++ BChat/Notifications/AppNotifications.swift
@@ -159,6 +159,7 @@ public class NotificationPresenter: NSObject, NotificationsProtocol {
     }
 
     public func notifyUser(for incomingMessage: TSIncomingMessage, in thread: TSThread, transaction: YapDatabaseReadTransaction) {
+        guard !thread.isArchived else { return }
         guard !thread.isMuted else { return }
         guard let threadId = thread.uniqueId else { return }
         let isMessageRequest = thread.isMessageRequest(using: transaction)
@@ -271,6 +272,7 @@ public class NotificationPresenter: NSObject, NotificationsProtocol {
     }
     
     public func notifyUser(forIncomingCall callInfoMessage: TSInfoMessage, in thread: TSThread, transaction: YapDatabaseReadTransaction) {
+        guard !thread.isArchived else { return }
         guard !thread.isMuted else { return }
         guard !thread.isGroupThread() else { return } // Calls shouldn't happen in groups
         guard let threadId = thread.uniqueId else { return }
@@ -304,6 +306,7 @@ public class NotificationPresenter: NSObject, NotificationsProtocol {
     }
     
     public func notifyUser(forReaction reactMessage: ReactMessage, in thread: TSThread, transaction: YapDatabaseReadTransaction) {
+        guard !thread.isArchived else { return }
         guard !thread.isMuted else { return }
         guard !thread.isGroupThread() else { return } // We do NOT notify emoji reacts in groups
         guard !thread.isMessageRequest(using: transaction) else { return }
