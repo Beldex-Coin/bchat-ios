28998227e603e87ce5b738c9124358ef3bb213df
acegreen1 <135817768+acegreen1@users.noreply.github.com>
2026-05-25 10:35:22 +0530

    Updated changelog

--- BChat/Onboarding/ChangeLogNewVC.swift
+++ BChat/Onboarding/ChangeLogNewVC.swift
@@ -18,7 +18,7 @@ class ChangeLogNewVC: BaseVC, UITableViewDataSource, UITableViewDelegate{
         return result
     }()
     
-    var versionArray = ["1.0.0", "1.1.0", "1.2.0", "1.2.1", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.4.0", "1.4.1", "1.4.2", "1.4.4", "1.4.5", "1.5.0", "1.6.0", "1.6.1", "1.6.2", "1.7.0", "1.7.1", "1.7.2", "1.7.3", "1.7.4"]
+    var versionArray = ["1.0.0", "1.1.0", "1.2.0", "1.2.1", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.4.0", "1.4.1", "1.4.2", "1.4.4", "1.4.5", "1.5.0", "1.6.0", "1.6.1", "1.6.2", "1.7.0", "1.7.1", "1.7.2", "1.7.3", "1.7.4", "1.7.5", "1.7.6"]
     var descArray = ["\u{2022} Initial release\n\u{2022} Added changelog",
                      "\u{2022} Message request implementation\n\u{2022} Link Preview will be turn on by default\n\u{2022} Add the images for SwipeActionsConfiguration",
                      "\u{2022} Call Feature Added\n\u{2022} Blocked Contact list added\n\u{2022} Minor Bug Fixes",
@@ -40,8 +40,10 @@ class ChangeLogNewVC: BaseVC, UITableViewDataSource, UITableViewDelegate{
                      "\u{2022} Minor bug fixes",
                      "\u{2022} Update for Hardfork 20 \n\u{2022} Minor bug fixes",
                      "\u{2022} Removed Wallet feature \n\u{2022} Minor bug fixes",
-                     "\u{2022} Minor bug fixes",]
-    var expandedRowArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+                     "\u{2022} Minor bug fixes",
+                     "\u{2022} Introduced Text formatting feature \n\u{2022} Minor bug fixes",
+                     "\u{2022} Crash and ANR fixes \n\u{2022} Text-formatting bug fixes \n\u{2022} Minor bug fixes",]
+    var expandedRowArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
     
     
     override func viewDidLoad() {
